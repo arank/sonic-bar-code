ff1eaf5806fec1def6f791df92c9f51d1fd942cf
arank <arankhanna@college.harvard.edu>
2014-05-04 22:17:48 -0400

    added media path

--- Project/urls.py
+++ Project/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from sonic_bar_code import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -9,4 +11,4 @@ urlpatterns = patterns('',
     url(r'^upload$', views.upload_file, ),
     url(r'^generate', views.generate_view, ),
     url(r'^sounds/(?P<id>[0-9]+)/', views.view_past, )
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
