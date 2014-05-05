74fd3fc74b0b00b081404d699055bcd4a2eb63c6
arank <arankhanna@college.harvard.edu>
2014-05-04 22:51:01 -0400

    added stuff

--- Project/urls.py
+++ Project/urls.py
@@ -10,9 +10,7 @@ urlpatterns = patterns('',
     url(r'^$', views.home, ),
     url(r'^upload$', views.upload_file, ),
     url(r'^generate', views.generate_view, ),
-    url(r'^sounds/(?P<id>[0-9]+)/', views.view_past, )
+    url(r'^sounds/(?P<id>[0-9]+)/', views.view_past, ),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT})
 )
 
-urlpatterns += patterns('',
-        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-        'document_root': settings.MEDIA_ROOT}))
